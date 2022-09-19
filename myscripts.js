17782a9903746dffd13be1eb92466db4f04c4b5d
evolF35 <maccyair@gmail.com>
2022-09-18 19:57:50 -0700

    Cant get the engine starting

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,33 @@
 
+const gameBoard = (function () {
+
+    let board = [];
+
+    const winningPositions = [[1,2,3],[1,5,9],[1,4,7]
+    [2,5,8],[3,6,9],[4,5,6],[7,8,9],[7,5,3]];
+
+    let checkWin = 0;
+
+
+    return(board);
+})();
+
+
+const flow = (function(){
+
+    const winningPositions = [[1,2,3],[1,5,9],[1,4,7]
+    [2,5,8],[3,6,9],[4,5,6],[7,8,9],[7,5,3]];
 
 
 
 
+})();
+
+let player = function (name) {
+
+
+    
 
 
 
-
-
+};
