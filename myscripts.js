2c48b273e8e4181ad979478153f9a800b6f5c41f
evolF35 <maccyair@gmail.com>
2022-09-18 17:02:33 -0700

    Reset

--- myscripts.js
+++ myscripts.js
@@ -1,21 +1,5 @@
-let Board = document.querySelector('#Board')
 
 
-function generateCleanBoard(){
-    Board.innerHTML = "";
-
-    for(let i = 0; i < 9; i++){
-        let box = document.createElement("div");
-        box.setAttribute("id",`box_${i}`);
-        box.classList.add("box");
-        Board.appendChild(box);
-    }
-}
-
-
-let GameBoard = (function (){
-
-})();
 
 
 
